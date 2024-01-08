887ea3265aa4aed705c20b91cef1c0820ad87dc0
James_css59 <89324782+Jmauricio59@users.noreply.github.com>
2024-01-07 20:32:19 -0600

    Update init.js
    Actualizacion en Login Google Firebase

--- init.js
+++ init.js
@@ -1,3 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     ,
@@ -10,4 +17,11 @@ const firebaseConfig = {
   };
   firebase.initializeApp(firebaseConfig);
   const auth=firebase.auth();
-  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
